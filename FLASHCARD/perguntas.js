ee53c4f1e2ce0c9b99e4b325a58a1ef1d8e0580e
maryferreir <0000110801396xsp@al.educacao.sp.gov.br>
2024-11-27 09:58:41 -0300

    Add files via upload

---
+++ FLASHCARD/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao (
+    'Química',
+    'Qual o nome popular do cloreto de sódio?',
+    'Sal de cozinha'
+)
+
+criaCartao (
+    'Geografia',
+    'Quanto tempo a Terra demora para dar uma volta completa em torno dela mesma?',
+    'Aproximadamente 24 horas'
+)
+
+criaCartao (
+    'História',
+   ' Em que país foi construído o Muro de Berlim?',
+    'Alemanha'
+)
